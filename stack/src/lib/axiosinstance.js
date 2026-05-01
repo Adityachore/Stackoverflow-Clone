e86baddcaa6f3bf99aa08da8df041835e74cfe05
Adityachore <adityachore9284@gmail.com>
2026-05-02 00:37:55 +0530

    final call

--- stack/src/lib/axiosinstance.js
+++ stack/src/lib/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.BACKEND_URL || "https://stackoverflow-clone-6cll.onrender.com",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "https://stackoverflow-clone-6cll.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
